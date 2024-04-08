ed9259b1db7ed477ac7e6dadd907585094f44ba6
vicky <vickyempire723@gmail.com>
2024-04-08 10:40:53 +0530

    created frontend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import { Hero } from "@/components/Hero";
 import Image from "next/image";
+import { Projects } from "@/components/Projects";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between bg-[#ece3ca]">
-      <div className="container"></div>
+      <div className="">
+        <Hero />
+        <Projects/>
+      </div>
+     
     </main>
   );
 }
---
+++ src/components/Projects.tsx
@@ -0,0 +1,303 @@
+export const Projects =()=>{
+    return(
+        <>
+
+<div className="grid grid-cols-4  gap-4">
+        <div>
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+            <div className="card-body">
+              <h2 className="card-title">
+                title!
+                <div className="badge badge-secondary">NEW</div>
+              </h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="grid grid-cols-2">
+                <div className="flex justify-center">
+                  <button className="">
+                    <span >
+                      <UpArrowIcon />12
+                    </span>
+                  </button>
+                </div>
+                <div className="flex justify-center">
+                  <button className="">
+                    <span>
+                      <DownArrowIcon />2
+                    </span>
+                  </button>
+                </div>
+              </div>
+              {/* The button to open modal */}
+              <label htmlFor="my_modal_7" className="btn text-black bg-white">Contribute</label>
+              <div className="text-center">
+              <a className="link link-info text-[#41ff54]">view Paper</a>
+            </div>
+            </div>
+          </div></div>
+
+        {/* <secondcard!!> */}
+        <div> <div className="card w-96 bg-base-100 shadow-xl">
+          <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+          <div className="card-body">
+            <h2 className="card-title">
+              title!
+              <div className="badge badge-secondary">NEW</div>
+            </h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="grid grid-cols-2">
+              <div className="flex justify-center">
+                <button className="">
+                  <span >
+                    <UpArrowIcon />18
+                  </span>
+                </button>
+              </div>
+              <div className="flex justify-center">
+                <button className="">
+                  <span>
+                    <DownArrowIcon />7
+                  </span>
+                </button>
+              </div>
+            </div>
+            <button className="btn btn-primary bg-white ">Contribute</button>
+            <div className="text-center">
+              <a className="link link-info text-[#41ff54]">view Paper</a>
+            </div>
+          </div>
+        </div></div>
+
+        {/* <thirdcard!!!> */}
+        <div> <div className="card w-96 bg-base-100 shadow-xl">
+          <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+          <div className="card-body">
+            <h2 className="card-title">
+              title!
+              <div className="badge badge-secondary">NEW</div>
+            </h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="grid grid-cols-2">
+              <div className="flex justify-center">
+                <button className="">
+                  <span >
+                    <UpArrowIcon />13
+                  </span>
+                </button>
+              </div>
+              <div className="flex justify-center">
+                <button className="">
+                  <span>
+                    <DownArrowIcon />6
+                  </span>
+                </button>
+              </div>
+            </div>
+            <button className="btn btn-primary bg-white ">Contribute</button>
+            <div className="text-center">
+              <a className="link link-info text-[#41ff54]">view Paper</a>
+            </div>
+          </div>
+        </div></div>
+
+        {/* <fourthcard!!!> */}
+        <div> <div className="card w-96 bg-base-100 shadow-xl">
+          <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+          <div className="card-body">
+            <h2 className="card-title">
+              title!
+              <div className="badge badge-secondary">NEW</div>
+            </h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div className="grid grid-cols-2">
+              <div className="flex justify-center">
+                <button className="">
+                  <span >
+                    <UpArrowIcon />14
+                  </span>
+                </button>
+              </div>
+              <div className="flex justify-center">
+                <button className="">
+                  <span>
+                    <DownArrowIcon />7
+                  </span>
+                </button>
+              </div>
+            </div>
+            <button className="btn btn-primary bg-white ">Contribute</button>
+            <div className="text-center">
+              <a className="link link-info text-[#41ff54]">view Paper</a>
+            </div>
+          </div>
+        </div></div>
+
+        {/* <second section!!!!!> */}
+      </div>
+      <div className="grid grid-cols-4 mt-28  gap-4">
+        <div>
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+            <div className="card-body">
+              <h2 className="card-title">
+                title!
+                <div className="badge badge-secondary">NEW</div>
+              </h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="grid grid-cols-2">
+                <div className="flex justify-center">
+                  <button className="">
+                    <span >
+                      <UpArrowIcon />11
+                    </span>
+                  </button>
+                </div>
+                <div className="flex justify-center">
+                  <button className="">
+                    <span>
+                      <DownArrowIcon />4
+                    </span>
+                  </button>
+                </div>
+              </div>
+              <button className="btn btn-primary bg-white ">Contribute</button>
+              <div className="text-center">
+                <a className="link link-info text-[#41ff54]">view Paper</a>
+              </div>
+            </div>
+          </div></div>
+
+        <div>
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+            <div className="card-body">
+              <h2 className="card-title">
+                title!
+                <div className="badge badge-secondary">NEW</div>
+              </h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="grid grid-cols-2">
+                <div className="flex justify-center">
+                  <button className="">
+                    <span >
+                      <UpArrowIcon />16
+                    </span>
+                  </button>
+                </div>
+                <div className="flex justify-center">
+                  <button className="">
+                    <span>
+                      <DownArrowIcon />7
+                    </span>
+                  </button>
+                </div>
+              </div>
+              <button className="btn btn-primary bg-white ">Contribute</button>
+              <div className="text-center">
+                <a className="link link-info text-[#41ff54]">view Paper</a>
+              </div>
+            </div>
+          </div></div>
+
+        <div>
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+            <div className="card-body">
+              <h2 className="card-title">
+                title!
+                <div className="badge badge-secondary">NEW</div>
+              </h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="grid grid-cols-2">
+                <div className="flex justify-center">
+                  <button className="">
+                    <span >
+                      <UpArrowIcon />18
+                    </span>
+                  </button>
+                </div>
+                <div className="flex justify-center">
+                  <button className="">
+                    <span>
+                      <DownArrowIcon />6
+                    </span>
+                  </button>
+                </div>
+              </div>
+              <button className="btn btn-primary bg-white ">Contribute</button>
+              <div className="text-center">
+                <a className="link link-info text-[#41ff54]">view Paper</a>
+              </div>
+            </div>
+          </div></div>
+
+        <div>
+          <div className="card w-96 bg-base-100 shadow-xl">
+            <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+            <div className="card-body">
+              <h2 className="card-title">
+                title!
+                <div className="badge badge-secondary">NEW</div>
+              </h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="grid grid-cols-2">
+                <div className="flex justify-center">
+                  <button className="">
+                    <span >
+                      <UpArrowIcon />19
+                    </span>
+                  </button>
+                </div>
+                <div className="flex justify-center">
+                  <button className="">
+                    <span>
+                      <DownArrowIcon />3
+                    </span>
+                  </button>
+                </div>
+              </div>
+              <button className="btn btn-primary bg-white ">Contribute</button>
+              <div className="text-center">
+                <a className="link link-info text-[#41ff54]">view Paper</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <input type="checkbox" id="my_modal_7" className="modal-toggle" />
+      <div className="modal" role="dialog">
+        <div className="modal-box">
+          <h3 className="text-lg font-bold">Hello!</h3>
+          <label className="input input-bordered flex items-center gap-2">
+            Name
+            <input type="text" className="grow" placeholder="Daisy" />
+          </label>
+         <button className="btn btn-primary bg-white my-6">click me!</button>
+        </div>
+
+        <label className="modal-backdrop" htmlFor="my_modal_7">Close</label>
+      </div>
+        </>
+    )
+    
+    
+}
+
+
+
+const UpArrowIcon = () => {
+    return (
+      <>
+        <svg width={32} fill="#000000" viewBox="0 0 24 24" id="up-arrow-circle" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg" className="icon flat-color"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><circle id="primary" cx="12" cy="12" r="10" style={{ fill: "#41ff54" }}></circle><path id="secondary" d="M14.83,9.5,12.69,6.38a.82.82,0,0,0-1.38,0L9.17,9.5A1,1,0,0,0,9.86,11H11v6a1,1,0,0,0,2,0V11h1.14A1,1,0,0,0,14.83,9.5Z" style={{ fill: "#ffffff" }}></path></g></svg>
+      </>
+    )
+  }
+  
+  const DownArrowIcon = () => {
+    return (
+      <>
+        <svg width={32} fill="#000000" viewBox="0 0 24 24" id="down-arrow-circle" data-name="Flat Color" xmlns="http://www.w3.org/2000/svg" className="icon flat-color"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><circle id="primary" cx="12" cy="12" r="10" style={{ fill: "#41ff54" }}></circle><path id="secondary" d="M14.14,13H13V7a1,1,0,0,0-2,0v6H9.86a1,1,0,0,0-.69,1.5l2.14,3.12a.82.82,0,0,0,1.38,0l2.14-3.12A1,1,0,0,0,14.14,13Z" style={{ fill: "#ffffff" }}></path></g></svg>
+      </>
+    )
+  }
